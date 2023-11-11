749d4041f6376393c5538536af2eb2eaffe27628
MateenCPH <cph-mr645@cphbusiness.dk>
2023-11-11 14:32:00 +0100

    Added FileIO

--- SP2_Mateen/src/main/java/org/example/FleetOfCars.java
+++ SP2_Mateen/src/main/java/org/example/FleetOfCars.java
@@ -1,25 +1,33 @@
 package org.example;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class FleetOfCars {
 
     protected ArrayList<Car> fleet = new ArrayList<>();
     protected TextUI ui = new TextUI();
+    protected FileIO io = new FileIO();
 
 
-    public void setup(){
-        ElectricCar e = new ElectricCar("ABC123", "Tesla", "Model X", 5, 400, 500);
-        GasolinCar g = new GasolinCar("DJ91100", "BMW", "530i", 4, 17);
-        DieselCar d = new DieselCar("CA39513","Mercedes","E300",4,16,true);
+    public void setup() {
+        //ElectricCar e = new ElectricCar("ABC123", "Tesla", "Model X", 5, 400, 500);
+        //GasolinCar g = new GasolinCar("DJ91100", "BMW", "530i", 4, 17);
+        //DieselCar d = new DieselCar("CA39513","Mercedes","E300",4,16,true);
 
-        addCar(e);
-        addCar(g);
-        addCar(d);
+        //addCar(e);
+        //addCar(g);
+        //addCar(d);
 
-        for (Car car : fleet) {
-            ui.displayMessage(car.toString());
+        ArrayList<String> data = io.readCarData("src/main/java/org/example/fuelcars.csv");
+        for (String s : data){
+            String[] row = s.split(",");
+
         }
+
+            for (Car car : fleet) {
+                ui.displayMessage(car.toString());
+            }
 
         getTotalRegistrationFeeForFleet();
         ui.displayMessage("Total fee=" + getTotalRegistrationFeeForFleet());
